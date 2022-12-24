bd22be7f82250211151370c4be7dd2bcf9bc2402
Shi_Sheng <ohayou@shisheng.icu>
2022-12-24 11:29:45 +0800

    SeTu_Go: Refine code

--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type RandomPost struct {
+	FileUrl string `json:"file_url"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/api/setu", func(w http.ResponseWriter, r *http.Request) {
+		tags := r.URL.Query().Get("tags")
+
+		url := "https://danbooru.donmai.us/posts/random.json"
+		if tags != "" {
+			url += "?tags=" + tags
+		}
+
+		client := http.Client{}
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			http.Error(w, "Error creating request", http.StatusInternalServerError)
+			return
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			http.Error(w, "Error fetching random post", http.StatusInternalServerError)
+			return
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				http.Error(w, "Error closing response body", http.StatusInternalServerError)
+				return
+			}
+		}(resp.Body)
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, "Error reading response body", http.StatusInternalServerError)
+			return
+		}
+
+		var post RandomPost
+		err = json.Unmarshal(body, &post)
+		if err != nil {
+			http.Error(w, "Error unmarshalling JSON", http.StatusInternalServerError)
+			return
+		}
+
+		if tags != "" {
+			log.Println("[INFO]\nTags:", tags, "\nUrl:", post.FileUrl, "\nUserAgent:", r.UserAgent(), "\nIP:", r.RemoteAddr)
+		} else {
+			log.Println("[INFO]\nTags: no", "\nUrl:", post.FileUrl, "\nUserAgent:", r.UserAgent(), "\nIP:", r.RemoteAddr)
+		}
+
+		http.Redirect(w, r, post.FileUrl, http.StatusFound)
+	})
+
+	log.Println("[INFO]\nStarted to listen on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 }
